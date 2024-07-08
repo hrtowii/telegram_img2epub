d609c3da2fa2a63a05e98bc41e28c4e55777b2d5
hrtowii <leonghongkit@gmail.com>
2024-07-08 23:48:16 +0800

    quick renaming

--- index.ts
+++ index.ts
@@ -1,29 +1,3 @@
-const url = process.env.API_URL
-const token = process.env.TOKEN
-const data = {
-  messaging_product: "whatsapp",
-  to: "6581899220",
-  type: "template",
-  template: {
-    name: "hello_world",
-    language: {
-      code: "en_US"
-    }
-  }
-};
-
-// fetch(url, {
-//   method: 'POST',
-//   headers: {
-//     'Authorization': `Bearer ${token}`,
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify(data)
-// })
-// .then(response => response.json())
-// .then(data => console.log(data))
-// .catch(error => console.error('Error:', error));
-
 import { Elysia } from 'elysia'
 
 new Elysia()
